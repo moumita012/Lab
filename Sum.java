6dd34a99fe8459b3ef0cb408c2eae6bbdedaba05
moumita012 <110465896+moumita012@users.noreply.github.com>
2022-08-12 17:58:09 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,20 @@
+//Q.5 Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+ 
+
+class Sum{
+public static void main(String...args){
+System.out.println("The two number is");
+int a =125;
+int b =24;
+System.out.println(a+ " " +b);
+int sum= a+b;
+int sub= a-b;
+int mul= a*b;
+double div= a/b;
+double reminder =a%b;
+System.out.println(" The sum is:"+sum);
+System.out.println(" The sub is:"+sub);
+System.out.println(" The mul is:"+mul);
+System.out.println(" The div is:"+div);
+System.out.println(" The reminder is:"+reminder);
+}}
